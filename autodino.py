22d591ca5b2b1ec9a868d1c86f8c0cddb779ea3c
Michael Fang <66933987+FangziOMG@users.noreply.github.com>
2021-04-05 15:55:34 +0800

    Add files via upload

---
+++ autodino.py
@@ -0,0 +1,36 @@
+'''
+Copyright © 2021 Michael Fang
+A python script that play the dino game on Chrome automatically.
+I AM ONLY 13!!
+GitHub: FangziOMG
+Follow me on twitter: @MichaelFangzi
+'''
+
+ PIL import ImageGrab
+from PIL import ImageOps
+import pyautogui
+import time
+from numpy import *
+
+restartbtn = (955, 490) # Location of the restart button
+dino = (625, 520) #location of the dinosaur
+
+def replay(): #replay
+    pyautogui.click(restartbtn)
+    print('Restarted Game')
+
+def detect(): #detect whether their is a obstacle
+    drange = (dino[0]+28, dino[1], dino[0]+136.5, dino[1]+5)
+    img = ImageGrab.grab(drange)
+    obs = ImageOps.grayscale(img)
+    arr = array(obs.getcolors())
+    return arr.sum()
+
+
+time.sleep(4) #Go to www.trex-game.skipser.com during this time
+replay()
+
+while True: #detect every single moment
+    detect()
+    if (detect() != 792): #check whether their is a obstacle
+        pyautogui.keyDown('space') #press spacebar (jump)
